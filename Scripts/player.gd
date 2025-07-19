c1bb288468825b730fca53e195a63b5d089e3eae
Bryceicle <bryce.gill@ontariotechu.net>
2025-07-19 15:20:14 -0400

    added gravity mechanic to physics

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,14 +5,33 @@ const SPEED = 100
 
 var direction
 
+var gravity_change = 1
+
+var gravity_sector = true 
+
+func rotate_char(gravity_change) -> void:
+	var rotate = create_tween()
+	if gravity_change == -1:
+		rotate.tween_property($".", "rotation_degrees", 180, 0.4) # Rotate to 180 degrees in 1 second
+	else:
+		rotate.tween_property($".", "rotation_degrees", 0, 0.4) # Rotate to 180 degrees in 1 second
+
 func _physics_process(delta: float) -> void:
 	# Gravity mechanic
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	
+	if Input.is_action_just_pressed("gravity_change") and gravity_sector:
+		gravity_change *= -1
+		rotate_char(gravity_change)
+	
+	if not is_on_floor() or gravity_change != 1:
+		velocity += get_gravity() * gravity_change * delta
 	
 	# Jump action
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("jump"):
+		if gravity_change == 1 and is_on_floor():
+			velocity.y = JUMP_VELOCITY * gravity_change
+		if gravity_change == -1 and is_on_ceiling():
+			velocity.y = JUMP_VELOCITY * gravity_change
 	
 	# Horizontal Movement
 	if Input.is_action_pressed("move_left"):
@@ -23,5 +42,6 @@ func _physics_process(delta: float) -> void:
 		direction = 0
 	
 	velocity.x = SPEED * direction
+	
 	
 	move_and_slide()
