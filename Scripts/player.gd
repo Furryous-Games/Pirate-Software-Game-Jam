fab5256b186377b25d1208f289ef5fc5a324d9d7
TekBot0507 <144193352+TekBot0507@users.noreply.github.com>
2025-07-19 15:28:27 -0400

    Camera Pan

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
+@onready var internal_collider_check: ShapeCast2D = $"Internal Collider Check"
+
 const JUMP_VELOCITY = -330
 const SPEED = 100
 
 var direction
 
 func _physics_process(delta: float) -> void:
+	# Do not process physics when inside of an object
+	if internal_collider_check.is_colliding():
+		position.y -= 5
+		
+		velocity = Vector2(0, 0)
+		return
+	
 	# Gravity mechanic
 	if not is_on_floor():
 		velocity += get_gravity() * delta
