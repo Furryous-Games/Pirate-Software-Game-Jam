2681c02c5af26c09805fe4230b355d61731acae0
Bryceicle <64384892+Bryceicle@users.noreply.github.com>
2025-07-22 15:46:29 -0400

    Movement v2 (#2)
    * Refined player movement
    
    - Refined player movement
    -- (dash, climb, and hold are in progress)
    - Added jump buffer
    - renamed files to follow standardized naming
    - Added Sector enum
    
    * Removed test code
    
    Removed test code accidentally left behind
    
    * player movement v2
    
    - Added acceleration & deceleration
    - Added climbing & wall jumping
    - Added dashing (Logistics only)
    - Added coyote time
    
    - Tweaked jump buffer
    - Tweaked gravity and jump velocity
    
    * movement v2.1
    
    Refined sector checks
    Refined velocity updates
    
    Reduced run speed (180 -> 160)
    Reduced dash velocity (440 -> 380)
    
    Edited code to be more readable
    
    ---------
    
    Co-authored-by: Chrono-Chord <micahlwalters1+git@gmail.com>

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,56 +1,85 @@
 extends CharacterBody2D
 
-@onready var internal_collider_check: ShapeCast2D = $"Internal Collider Check"
+const RUN_SPEED = 160
+const CLIMB_SPEED = -120
+const ACCELERATION = 1000
+const DECELERATION = 1500
+const DASH_VELOCITY = 380
+const JUMP_VELOCITY = -420
+const GRAVITY = 1440 # get_gravity().y * 1.6
 
-const JUMP_VELOCITY = -330
-const SPEED = 100
+var can_dash := false
 
-var direction
+@onready var jump_buffer: Timer = $JumpBuffer
+@onready var coyote_time: Timer = $CoyoteTime
+@onready var dash_time: Timer = $DashTime
 
-var gravity_change = 1
+@onready var Sector = $"../".Sector
+@onready var current_sector = $"../".current_sector
 
-var gravity_sector = true 
-
-func rotate_char(gravity_change) -> void:
-	var rotate = create_tween()
-	if gravity_change == -1:
-		rotate.tween_property($".", "rotation_degrees", 180, 0.4) # Rotate to 180 degrees in 1 second
-	else:
-		rotate.tween_property($".", "rotation_degrees", 0, 0.4) # Rotate to 180 degrees in 1 second
 
 func _physics_process(delta: float) -> void:
-	# Do not process physics when inside of an object
-	if internal_collider_check.is_colliding():
-		position.y -= 5
+	var direction := Vector2(
+			Input.get_axis("move_left", "move_right"), 
+			Input.get_axis("move_up", "move_down")
+	)
+	var climbing: bool = Input.is_action_pressed("climb") and is_on_wall() 
+	# NOTE: To climb, you must hold direction.x
+	
+	# Reset dash, Logistics only
+	if (
+			current_sector == Sector.LOGISTICS
+			and is_on_floor()
+			and not can_dash
+			and dash_time.is_stopped()
+	):
+		can_dash = true
+	
+	# Coyote time
+	if not (
+			velocity.y 
+			or is_on_floor()
+			or is_on_wall()
+	):
+		coyote_time.start()
+	
+	# Update velocity
+	if dash_time.is_stopped(): # Prevents velocity change while dashing
+		var desired_velocity := Vector2(
+				# x:
+				RUN_SPEED * direction.x * int(not climbing),
+				# y:
+				0.0 if is_on_floor()
+				else CLIMB_SPEED * -direction.y if climbing
+				else velocity.y + GRAVITY * delta
+		)
+		var velocity_acceleration: int = ACCELERATION if sign(velocity.x * direction.x) == 1 else DECELERATION
 		
-		velocity = Vector2(0, 0)
-		return
-	
-	# Gravity mechanic
-	
-	if Input.is_action_just_pressed("gravity_change") and gravity_sector:
-		gravity_change *= -1
-		rotate_char(gravity_change)
-	
-	if not is_on_floor() or gravity_change != 1:
-		velocity += get_gravity() * gravity_change * delta
+		velocity = Vector2(move_toward(velocity.x, desired_velocity.x, velocity_acceleration * delta), desired_velocity.y)
 	
 	# Jump action
-	if Input.is_action_pressed("jump"):
-		if gravity_change == 1 and is_on_floor():
-			velocity.y = JUMP_VELOCITY * gravity_change
-		if gravity_change == -1 and is_on_ceiling():
-			velocity.y = JUMP_VELOCITY * gravity_change
+	if (
+			Input.is_action_just_pressed("jump")
+			or is_on_floor() and not jump_buffer.is_stopped()
+	):
+		if is_on_floor() or not coyote_time.is_stopped():
+			velocity.y = JUMP_VELOCITY
+			jump_buffer.stop()
+		else:
+			jump_buffer.start()
+			# Wall jump
+			if is_on_wall():
+				velocity.y = JUMP_VELOCITY
+				velocity.x = RUN_SPEED * get_wall_normal().x
 	
-	# Horizontal Movement
-	if Input.is_action_pressed("move_left"):
-		direction = -1
-	elif Input.is_action_pressed("move_right"):
-		direction = 1
-	else:
-		direction = 0
-	
-	velocity.x = SPEED * direction
-	
+	# Dash action, Logistics only
+	if (
+			current_sector == Sector.LOGISTICS
+			and Input.is_action_just_pressed("dash")
+			and can_dash
+	):
+		velocity = round(Vector2.from_angle(direction.angle()) * DASH_VELOCITY) # Corrects diagonal dashing
+		dash_time.start()
+		can_dash = false
 	
 	move_and_slide()
