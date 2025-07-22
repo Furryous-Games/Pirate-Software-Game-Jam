52a4a57c00d699027e8ab7bee4a5201f9fdb7a00
Bryceicle <bryce.gill@ontariotechu.net>
2025-07-22 15:59:57 -0400

    Squashed commit of the following:
    commit c753e0c19518a8310a2a542ac9377307b40d6c87
    Author: TekBot0507 <144193352+TekBot0507@users.noreply.github.com>
    Date:   Tue Jul 22 15:44:26 2025 -0400
    
        Engineering Sector Development v3.3
    
    commit 5cf4470603548bba029c89faa2e9fa87e7b2e495
    Author: TekBot0507 <144193352+TekBot0507@users.noreply.github.com>
    Date:   Tue Jul 22 14:14:52 2025 -0400
    
        Engineering Sector Development v3.2
    
    commit a8b708b4013a34ebb5f994030f8bc276c771d834
    Author: TekBot0507 <144193352+TekBot0507@users.noreply.github.com>
    Date:   Tue Jul 22 14:13:44 2025 -0400
    
        Engineering Sector Development v3.1
    
    commit 3b09693b8640d7459199dd87ac250c2b488e3de4
    Author: TekBot0507 <144193352+TekBot0507@users.noreply.github.com>
    Date:   Mon Jul 21 23:47:23 2025 -0400
    
        Engineering Sector Development 3
    
    commit 9939ca99f0cda2f2d5e294ea40976a7637112927
    Author: TekBot0507 <144193352+TekBot0507@users.noreply.github.com>
    Date:   Mon Jul 21 02:11:06 2025 -0400
    
        Further Engineering Sector Development
    
    commit 58562513824e8ddfd7b6dc34e8eac4c7ad2246da
    Author: TekBot0507 <144193352+TekBot0507@users.noreply.github.com>
    Date:   Sat Jul 19 19:47:54 2025 -0400
    
        Initial Development for Engineering Sector

---
+++ Scenes/timing_mechanism_platform.gd
@@ -0,0 +1,118 @@
+extends AnimatableBody2D
+
+@export var max_distance: Vector2i
+@export var start_distance: Vector2i
+
+@export var initial_direction: Vector2i = Vector2i(1, 1)
+@export var step_size: int
+@export var platform_size: Vector2i
+
+@export var instant_return: bool
+
+@onready var platform_sprite: TileMapLayer = $"Platform Visual"
+@onready var platform_collider: CollisionShape2D = $"Platform Collider"
+
+
+var original_position
+
+var curr_distance = Vector2i(0, 0)
+var curr_vertical_distance = 0
+var curr_direction = Vector2i(1, 1)
+
+
+func _ready() -> void:
+	original_position = position
+	
+	# Set the directions to 0 if the platform doesnt move on the axis
+	if max_distance.x == 0:
+		curr_direction.x = 0
+	if max_distance.y == 0:
+		curr_direction.y = 0
+	
+	# Set the step size and initial direction
+	curr_direction *= step_size
+	curr_direction *= initial_direction
+	
+	# Expand the platform to its set size
+	expand_platform(platform_size)
+	
+	# Move the position to its starting distance
+	if start_distance != Vector2i(0, 0):
+		warp_to_position(start_distance, 0)
+	
+	# If the platform is set to instant return, add padding to its max distances
+	if instant_return:
+		if max_distance.x < 0:
+			max_distance.x -= step_size
+		if max_distance.x > 0:
+			max_distance.x += step_size
+		if max_distance.y < 0:
+			max_distance.y -= step_size
+		if max_distance.y > 0:
+			max_distance.y += step_size
+		
+		print(curr_distance)
+
+func expand_platform(new_size: Vector2i) -> void:
+	for x in range(platform_size.x):
+		for y in range(platform_size.y):
+			platform_sprite.set_cell(Vector2i(x, y), 1, Vector2i(14, 3))
+	
+	platform_collider.shape.size = platform_size * 20
+	platform_collider.position = (platform_size - Vector2i.ONE) * 10
+
+
+func warp_to_position(new_pos: Vector2i, time_to_move: float, disable_collider: bool = true) -> void:
+	# Reset the sprite's distance
+	curr_distance = new_pos
+	
+	# If disable collider is true, then move the platform with no collider
+	if disable_collider:
+		# Makes the mechanism temporarily not collide with anything
+		platform_collider.disabled = true
+		
+		# Delays the reenabling of the collider
+		get_tree().create_timer(time_to_move + 0.1).timeout.connect(enable_collider)
+	
+	# Move the sprite
+	var tween = create_tween()
+	tween.tween_property($".", "position", Vector2(original_position.x + (20 * curr_distance.x), original_position.y + (20 * curr_distance.y)), time_to_move)
+
+
+func _on_timing_mechanism_tick() -> void:
+	var move_platform = true
+	
+	# If the platform has hit either edge of its rail on the horizontal axis, then flip its direction
+	if curr_distance.x + curr_direction.x < min(0, max_distance.x) or curr_distance.x + curr_direction.x > max(0, max_distance.x):
+		# Handles instant return
+		if instant_return and curr_distance.x != 0:
+			# Warp the platform to the new position
+			warp_to_position(Vector2i(0, curr_distance.y), 0)
+			move_platform = false
+		else:
+			curr_direction.x *= -1
+	
+	# If the platform has hit either edge of its rail on the vertical axis, then flip its direction
+	if curr_distance.y + curr_direction.y < min(0, max_distance.y) or curr_distance.y + curr_direction.y > max(0, max_distance.y):
+		# Handles instant return
+		if instant_return and curr_distance.y != 0:
+			# Warp the platform to the new position
+			warp_to_position(Vector2i(curr_distance.x, 0), 0)
+			move_platform = false
+		else:
+			curr_direction.y *= -1
+	
+	# Move the sprite
+	if move_platform:
+		var tween = create_tween()
+		tween.tween_property($".", "position", Vector2(original_position.x + (20 * curr_distance.x), original_position.y + (20 * curr_distance.y)), 0.15)
+	
+	# Update the current horizontal distance
+	curr_distance += curr_direction
+	
+	# Enable the collider if disabled
+	#if platform_collider.disabled:
+		#platform_collider.disabled = false
+
+func enable_collider() -> void:
+	platform_collider.disabled = false
---
+++ Scripts/wall_terminal.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+@onready var key_prompt: Label = $"Key Prompt"
+
+
+func interact_with_terminal() -> void:
+	print("INTERACT")
+	
+
+func enable_terminal() -> void:
+	# Make the interact prompt visible
+	key_prompt.visible = true
+
+func disable_terminal() -> void:
+	# Make the interact prompt hidden
+	key_prompt.visible = false
+
+
+# Called when the player enters into the radius of the terminal
+func _on_body_entered(body: Node2D) -> void:
+	# Check if the body type is CharacterBody2d (the player)
+	if body is CharacterBody2D:
+		# Add self to the current terminals that the player is close to
+		body.current_terminals.append(self)
+
+
+# Called when the player exists the radius of the terminal
+func _on_body_exited(body: Node2D) -> void:
+	# Check if the body type is CharacterBody2d (the player)
+	if body is CharacterBody2D:
+		disable_terminal()
+		
+		# Remove self from the current terminals that the player is close to
+		if self in body.current_terminals:
+			body.current_terminals.erase(self)
