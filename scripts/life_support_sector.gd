9057e9c9af5519b791d6d8bb70f5f9cd2bac31e9
Bryceicle <bryce.gill@ontariotechu.net>
2025-07-24 00:26:41 -0400

    Squashed commit of the following:
    commit 8867834a7fc21c6d13d8bca71446f71ab70989d5
    Author: Bryceicle <bryce.gill@ontariotechu.net>
    Date:   Thu Jul 24 00:19:10 2025 -0400
    
        added respawn, updated life support
    
        - added re spawn mechanic based off of camera position
    
        - edited death block layer
    
        -edited water block layer
    
    commit 541ed786b136a93836642b570a2b349ea7f1536b
    Merge: e353645 1011f5c
    Author: Bryceicle <bryce.gill@ontariotechu.net>
    Date:   Wed Jul 23 22:02:26 2025 -0400
    
        Merge branch 'main' into life_support_update
    
    commit e353645612a77f4c3ca6f5eb527e3f1c8b75ed2f
    Author: Bryceicle <bryce.gill@ontariotechu.net>
    Date:   Wed Jul 23 22:01:17 2025 -0400
    
        life_support & gravity_change update
    
        - updateed maptiles for life_support
           - re did t1 and t2, updated p1

---
+++ scripts/life_support_sector.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+const ROOM_SPAWN_DATA = {
+		Vector2i(0,0): Vector2i(0,300),
+		Vector2i(1,0): Vector2i(580, 300),
+		Vector2i(2,0): Vector2i(1240, 200),
+		Vector2i(2,1): Vector2i(1240, 200),
+		Vector2i(2,-1): Vector2i(1240, 200),
+}
