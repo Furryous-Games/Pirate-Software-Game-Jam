daea03f233e20a70dfabd06a6db23a1f6bb45390
Bryceicle <64384892+Bryceicle@users.noreply.github.com>
2025-07-30 01:40:31 -0400

    Administration Effects HUD (#34)
    Co-authored-by: TekBot0507 <144193352+TekBot0507@users.noreply.github.com>

---
+++ scripts/ability_hud.gd
@@ -0,0 +1,44 @@
+extends VBoxContainer
+
+@onready var effects_node: VBoxContainer = $"."
+
+var all_curr_abilities: Dictionary = {
+	
+}
+
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	# Updates each status effect's value
+	for ability_name in all_curr_abilities:
+		all_curr_abilities[ability_name]["label"]
+		all_curr_abilities[ability_name]["timer"]
+		
+		# If the timer instance is invalid, remove the ability
+		if not is_instance_valid(all_curr_abilities[ability_name]["timer"]):
+			all_curr_abilities[ability_name]["label"].queue_free()
+			continue
+		
+		# Hides the status effect if its not visible
+		if all_curr_abilities[ability_name]["timer"].is_stopped():
+			all_curr_abilities[ability_name]["label"].visible = false
+		else:
+			all_curr_abilities[ability_name]["label"].visible = true
+		
+		# Gets the rounded time left on the ability
+		var time_left = snappedf(all_curr_abilities[ability_name]["timer"].time_left, 0.01)
+		
+		# Updates the text
+		all_curr_abilities[ability_name]["label"].text = ability_name + ": " + ("0" if time_left < 10.00 else "") + ("%.2f" % time_left) + " "
+
+
+func make_new_ability(ability_name: String, new_timer: Timer = null) -> void:
+	# Make a new label
+	var new_effect = preload("res://scenes/effect_label.tscn").instantiate()
+	effects_node.add_child(new_effect)
+	
+	all_curr_abilities[ability_name] = {
+		"label": new_effect,
+		"timer": new_timer,
+	}
