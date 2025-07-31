eda174cae95519fae2df93ff3142c60f095cce63
Chrono-Chord <micahlwalters1+git@gmail.com>
2025-07-31 07:34:38 -0400

    Title terminal v4

--- scripts/administrative_sector_officer.gd
+++ scripts/administrative_sector_officer.gd
@@ -158,7 +158,6 @@ func _gravity_flip(wait_time: int = 10) -> void:
 	# Restart the timer
 	timed_gravity_flip.start()
 func _gravity_flip_timeout() -> void:
-	print("FLIP BACK")
 	if main_script.player.gravity_change == -1:
 		# Flip the gravity back
 		main_script.player.gravity_invert()
@@ -206,7 +205,6 @@ func _officer_terminal_interacted():
 				update_officer_task()
 
 func _officer_battle_timeout():
-	print("RESET")
 	if not officer_battle_complete:
 		# Kill the player
 		main_script.player.death()
@@ -258,7 +256,6 @@ func toggle_doors(enabled):
 
 ## TASKS
 func _on_complete_task(task_name: String) -> void:
-	print("TASK COMPLETE: ", task_name)
 	
 	# Make note that the task is complete
 	all_tasks_complete[task_name] = true
@@ -280,7 +277,6 @@ func update_officer_task() -> void:
 		check_tasks_complete()
 
 func check_tasks_complete() -> void:
-	print(all_tasks_complete)
 	
 	if officer_battle_ongoing == true:
 		for task in all_tasks_complete:
@@ -297,9 +293,6 @@ func check_tasks_complete() -> void:
 		
 		# Slow down the tick speed of the timing mechanisms
 		get_node("../Timing Mechanisms").wait_time /= 0.75
-		
-		print("ALL TASKS OK")
-		print(all_tasks_complete)
 		
 		officer_battle_ongoing = false
 		officer_battle_complete = true
